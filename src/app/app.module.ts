61a4dff2862f9ded4179408e7184c47fc36c70ee
abdo00o00 <rahma.abdallah.eloo00o@gmail.com>
2024-08-07 21:03:55 +0300

    add project angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
